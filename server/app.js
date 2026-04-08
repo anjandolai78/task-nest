8ce8f8df7f0349bc85698631f880639237d1777a
Anjan <anjandolai78@gmail.com>
2026-04-08 17:55:33 +0530

    fix: bind to 0.0.0.0 for render

--- server/app.js
+++ server/app.js
@@ -27,6 +27,6 @@ app.use("/api/v1", taskApis);
 
 const PORT = process.env.PORT || 1000;
 
-app.listen(PORT, () => {
+app.listen(PORT, "0.0.0.0", () => {
   console.log(`Server started at PORT = ${PORT}`);
 });
