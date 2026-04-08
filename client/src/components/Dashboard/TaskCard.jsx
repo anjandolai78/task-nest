529d19cad1956cc5d3571b0e50a967ef7ee00217
Anjan <anjandolai78@gmail.com>
2026-04-08 20:20:37 +0530

    fix priority

--- client/src/components/Dashboard/TaskCard.jsx
+++ client/src/components/Dashboard/TaskCard.jsx
@@ -6,11 +6,13 @@ const TaskCard = ({data}) => {
    window.sessionStorage.setItem("editTaskId",id);
    window.location.reload();
   };
-   const formattedDate = data.dueDate ? new Date(data.dueDate).toLocaleDateString("en-GB", {
+
+  const formattedDate = data.dueDate ? new Date(data.dueDate).toLocaleDateString("en-GB", {
     day: "2-digit",
     month: "short",
     year: "numeric",
   }) : null;
+
   const formattedCreatedAt = data.createdAt ? new Date(data.createdAt).toLocaleDateString("en-GB", {
     day: "2-digit",
     month: "short",
@@ -23,9 +25,9 @@ const TaskCard = ({data}) => {
       <div className="flex items-center justify-between">
         <h1 className="">{data.title}</h1>
 
-        <span className={`text-xs ${data.priority === "low" ? "text-green-600 bg-green-100" : data.priority === "medium" ? "text-yellow-600 bg-yellow-100" : "text-red-600 bg-red-100"}   px-2 py-0.5 rounded-full`}>
-       {data.priority}
-     </span>
+        <span className={`text-xs ${data.priority === "low" ? "text-green-600 bg-green-100" : data.priority === "medium" ? "text-yellow-600 bg-yellow-100" : "text-red-600 bg-red-100"} px-2 py-0.5 rounded-full`}>
+          {data.priority.charAt(0).toUpperCase() + data.priority.slice(1)}
+        </span>
       </div>
 
       <hr className="my-2" />
@@ -33,11 +35,13 @@ const TaskCard = ({data}) => {
       <p className="text-sm text-zinc-500 text-start">
         {data.description}
       </p>
-        {formattedDate && (
+
+      {formattedDate && (
         <p className="text-xs text-zinc-400 mt-1 text-start">
           Due: {formattedDate}
         </p>
       )}
+      
       {formattedCreatedAt && (
         <p className="text-xs text-zinc-400 mt-1 text-start">
           Created: {formattedCreatedAt}
