08497c0ff452997acd6df9b367c87e289ca4e01f
Anjan <anjandolai78@gmail.com>
2026-04-08 18:09:54 +0530

    fix: remove cors crash issue

--- server/app.js
+++ server/app.js
@@ -1,31 +1,35 @@
 const express = require("express");
 const app = express();
 app.set("trust proxy", 1);
+
 const cors = require("cors");
 const cookieParser = require("cookie-parser");
 require("dotenv").config();
 require("./connection/conn");
-const userApis = require("./controllers/user");
- const taskApis = require("./controllers/task")
 
+const userApis = require("./controllers/user");
+const taskApis = require("./controllers/task");
+
+// Middleware
 app.use(express.json());
+
 app.use(cors({
   origin: "https://task-nest-frontend.onrender.com",
-  credentials: true,
-  methods: ["GET", "POST", "PUT", "DELETE"],
-  allowedHeaders: ["Content-Type", "Authorization"]
+  credentials: true
 }));
-app.options("*", cors());
+
 app.use(cookieParser());
+
+// Test route
 app.get("/", (req, res) => {
   res.send("Hello from backend");
 });
 
-
-// Mount the router
+// Routes
 app.use("/api/v1", userApis);
 app.use("/api/v1", taskApis);
 
+// Server
 const PORT = process.env.PORT || 1000;
 
 app.listen(PORT, "0.0.0.0", () => {
